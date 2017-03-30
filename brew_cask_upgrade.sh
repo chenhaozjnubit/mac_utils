e54c76248d2034870a80e88917ee6f9a42e431cc
chenhao <hao.chen@eayun.com>
2017-03-30 14:34:49 +0800

    just like brew update ; brew upgrade

---
+++ brew_cask_upgrade.sh
@@ -0,0 +1,17 @@
+for name in `brew cask list`
+do
+	echo $name
+	s1=`brew cask info $name|grep $name":"`
+	s2=`brew cask info $name|grep "/usr/local/Caskroom/"$name`
+	s_remote=${s1#*:}
+	echo "remote:\t"$s_remote
+	s_local=${s2##*/}
+	s_local=${s_local%(*}
+	echo "local:\t"${s_local}
+	if [ $s_remote = $s_local ]; then
+		echo "need not reinstall"
+	else
+		echo "need reinstall "$name "!"
+		brew cask reinstall $name
+	fi
+done
